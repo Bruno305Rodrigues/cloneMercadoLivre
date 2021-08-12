caebe4ffaf7ba3291ca32504f8a9e8b2b064616f
Bruno305Rodrigues <bruno_lmr_op@hotmail.com>
2021-08-12 06:57:30 -0300

    finalizando layout da página

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Footer from "../Footer";
+import Header from "../Header";
 
 import Product from '../Product';
 import { Container, Wrapper } from './styles';
@@ -6,9 +8,9 @@ import { Container, Wrapper } from './styles';
 function Layout () {
     return (
         <Container>
-           
+           <Header />
                 <Product />
-            
+            <Footer />
         </Container>
     );
 };
